12d31fcd2ecfebc58f77f8c42a230ae4c2b694a9
Step-henC <stevecunningham@outlook.com>
2024-04-05 09:22:14 -0500

    add readme and check for env for page load

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -10,9 +10,27 @@ import faker from 'faker';
 //of shared declaration in ModuleFederationPlugin
 //adding carats in package.json will make all major releases acceptable to use
 
-//we can load faker in browser multiple times
-//but cannot load React in browser or else get an error
 
+
+// let products = "";
+
+// for (let i = 0; i< 5; i++){
+//     const name = faker.commerce.productName();
+
+//     products += `<div>${name}</div>`;
+// }
+
+//document.querySelector('#dev-products').innerHTML = products;
+// need to add div with classname inside container index.html
+
+//the querySelector is not good, because it assumes the name of div in container.
+    //container theoretically managed by other team
+
+//Situation 1: 
+    //Local development with DEFINITE element in th index.html
+
+    const mount = (htmlElement) => { //arbitrary function name
+        
 let products = "";
 
 for (let i = 0; i< 5; i++){
@@ -21,5 +39,26 @@ for (let i = 0; i< 5; i++){
     products += `<div>${name}</div>`;
 }
 
-document.querySelector('#dev-products').innerHTML = products;
-// need to add div with classname inside container index.html
+htmlElement.innerHTML = products;
+    };
+
+    if (process.env.NODE_ENV === 'development') {//we added in mode of development in webpack config
+                                                //so webpack provides this
+                                                //but we could be in development mode with container so need another check
+        const element = document.querySelector('#dev-products');
+
+        //we assume dev-products id is only local. not good assumption could be in container too
+        //but, we can always change local name to make it unique
+        if (element){
+            mount(element); //if we found element, we are probably in development (local)
+
+        }
+    }
+//Situation 2: 
+    //Running in production through container app with 
+    //No guarantee that Element in index.html
+    //do not Immediately render app, we want asynchronous
+export {mount}; //container can call mount function whenever it wants now
+
+//NOW since we added mount in bootstrap, we do not want to give ModuleFederationPlugin 
+// the index.js file, we want to give them the bootstrap since it is exporting the mount function
--- products/src/index.js
+++ products/src/index.js
@@ -1,1 +1,16 @@
 import('./bootstrap.js')
+
+//we can load faker in browser multiple times
+//but cannot load React in browser or else get an error
+
+//to prevent multiple browser loads, 
+//change webpack.config.js "shared" from a list of package strings
+        //ie. shared: ["faker"]
+//to an object for singleton like it is now
+        // shared: { 
+            //faker: {
+                //singleton: true
+          //  }
+       // }
+
+       //Be mindful of singleton rule when working with diff versions
